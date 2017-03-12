eb5e037a6f6fe381aaed5f66d36178350c3ce63f
mech <mechelle.niezas@infinit-o.com>
2017-03-13 00:38:19 +0800

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
 import { GeevlyService } from '../../providers/geevly-service';
 import { LoginPage } from '../login/login';
@@ -14,26 +14,66 @@ export class HomePage {
 	username = '';
 	selected = '';
 	donees = [];
+	filteredItems:any;
+	loading: Loading;
 
-	constructor(private navCtrl: NavController, private _auth: AuthService, private _geevly : GeevlyService) {
+	constructor(private navCtrl: NavController, private _auth: AuthService, private _geevly : GeevlyService, private alertCtrl: AlertController, private loadingCtrl: LoadingController) {
 		// let info = this._auth.getUserInfo();
 		// console.log(info);
 		// this.name = info.name;
 		// this.username = info.username;
 		this.selected = 'unclaimed';
+		this.fetchData();
+  	}
 
-		for(let i =0; i < 10; i++) {
-			this.donees.push(i);
-		}
-
-		this._geevly.getDonees().subscribe((data) => {
+  	fetchData() {
+  		this.showLoading()
+		this._geevly.getDonees().subscribe(data => {
+			this.donees = data.result;
+			this.loading.dismiss();
 			console.log(data);
-			alert(JSON.stringify(data));
 		}, (error) => {
 			console.log(JSON.stringify(error));
 		});
-
-
   	}
+
+  	reset() {
+  		this.filteredItems = this.donees;
+  	}
+
+  	getItems(ev) {
+	    // Reset items back to all of the items
+	    this.reset();
+
+	    // set val to the value of the ev target
+	    var val = ev.target.value;
+
+	    // if the value is an empty string don't filter the items
+	    if (val && val.trim() != '') {
+	      this.filteredItems = this.filteredItems.filter((item) => {
+	        return (item.LastName.toLowerCase().indexOf(val.toLowerCase()) > -1);
+	      })
+	    }
+	  }
+
+  	assignCopy(){
+	   this.filteredItems = Object.assign([], this.donees);
+	}
+
+  	filterItem(value){
+  		console.log(value);
+	   if(!value) this.assignCopy(); //when nothing has typed
+	   this.filteredItems = Object.assign([], this.donees).filter(
+	      // item => item.LastName.toLowerCase().indexOf(value.toLowerCase()) > -1
+	      item => console.log(item.LastName)
+	   )
+	}
+
+  	showLoading() {
+		this.loading = this.loadingCtrl.create({
+      		content: 'Please wait...'
+	    });
+	    this.loading.present();
+	}
 
 }
--- src/providers/geevly-service.ts
+++ src/providers/geevly-service.ts
@@ -11,24 +11,14 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GeevlyService {
 
-  	constructor(public http: Http) {
-    	console.log('Hello GeevlyService Provider');
-	}
+  key = '8a04542c78cd8ded94df700b2f73e49f33bc613f';
+
+  constructor(public http: Http) {
+  	console.log('Hello GeevlyService Provider');
+  }
 
 	getDonees() {
-      // let headers = new Headers();
-      // headers.append('GEEVLY-API-KEY','8a04542c78cd8ded94df700b2f73e49f33bc613f');
-      // headers.append('Content-type','application/x-www-form-urlencoded');
-      // return this.http.get('https://api.geevly.org/api_donee/donees', { headers: headers }).map((res) => res.json());
-
-
-       /******* NEW ********/
-      let params = JSON.stringify({"username": "store2", "password": "kernel32"});
-      // let headers = new Headers({ 'Content-Type': 'application/json' });
-      let headers = new Headers({ 'GEEVLY-API-KEY': '8a04542c78cd8ded94df700b2f73e49f33bc613f', 'Content-Type': 'application/json' });
-      // let headers = new Headers({ 'Content-Type': 'multipart/form-data' });
-      let options = new RequestOptions({  headers: headers });
-      return this.http.get('https://api.geevly.org/api_donee/donees', options).map((res) => {res.json()});
+      return this.http.get('https://api.geevly.org/api_donee/donees?GEEVLY-API-KEY=' + this.key).map(res => res.json());
     }
 
 }
